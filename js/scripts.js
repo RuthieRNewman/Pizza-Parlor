976abfb4cd480d87eb1f2ee8f23d49d619cc6fc8
Ruthie Newman <ruthienewman@protonmail.com>
2020-06-19 16:02:28 -0700

    add branching statement which assigns a number value to each topping selection selected by the user.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
 //business logic fo pizza
-function Pizza (size, toppings, total) {
+function Pizza (size, toppings) {
   this.size = {
     size1 : "small",
     size2 : "large" 
@@ -8,32 +8,32 @@ function Pizza (size, toppings, total) {
   this.toppings = {
     'topping1' : "olives",
     'topping2' : "mushrooms"
+  }
 };
-this.total = total
+
+Pizza.prototype.totalToppings = function(userPizzaToppings) { 
 }
 
-
-Pizza.prototype.totalCost = function(size, toppings) {
-  
-  }
 
 
 
 //UI Logic
 $(document).ready(function(){ 
- 
   $("form#pizzaSizeAndToppings").submit(function(event){
     event.preventDefault();
       
     const userPizzaSize = $("#pizza-size").val(); 
+    console.log(userPizzaSize);
     if (userPizzaSize === "small") {
       let baseCost = 15
       let baseCostInteger = parseInt(baseCost);
       console.log(baseCostInteger);
+      
     }else if (userPizzaSize === "large") {
       let baseCost = 20
       let baseCostInteger = parseInt(baseCost);
       console.log(baseCostInteger);
+      
     }
 
     let userPizzaToppings = [];
@@ -41,9 +41,15 @@ $(document).ready(function(){
     const userPizzaToppingSelection = $(this).val();
     let userPizzaToppings = userPizzaToppingSelection;
     console.log(userPizzaToppings);
-    })
-    let newUserPizza = new Pizza(userPizzaSize, userPizzaToppings,0)
-
-
+      if (userPizzaToppingSelection === "olives"){
+      let topping1 = 1
+      console.log(topping1);
+      }else if (userPizzaToppingSelection === "mushrooms"){
+      let topping2 = 2
+      console.log(topping2);
+      }
+  });
+    let newUserPizza = new Pizza(userPizzaSize, userPizzaToppings)
+    console.log(newUserPizza.toppings.topping1)
     }); 
   });
