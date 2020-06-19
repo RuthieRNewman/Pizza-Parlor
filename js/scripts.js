5ebfea9f7a0c5febb5fcd12fe5dde090d4aac6bb
Ruthie Newman <ruthienewman@protonmail.com>
2020-06-19 14:23:07 -0700

    add object to object constuctor key:value pair, 'this.toppings, add 'total' as a key:value pair to Pizza constructor.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,36 @@
-//business logic
 
-function Pizza (size, toppings, finalPizza) {
-  this.size = size
-  this.toppings = [];
-  this.finalPizza = [];
-}
-
-Pizza.prototype.size = function(size){
-  if (userPizzaSize === 1){
-  let basePrice = 15
-}else if (userPizzaSize === 2) {
-  let basePrice = 20
-}
-}
-
-Pizza.prototype.toppings = function(toppings) {
+//business logic fo pizza
+function Pizza (size, toppings, total) {
+  this.size = size;
+  this.toppings = {
+    'topping1' : "olives",
+    'topping2' : "mushrooms"
+};
+this.total = total
 }
 
 
-
+Pizza.prototype.sizeCost = function(size) {
+  }
 
 
 
 //UI Logic
-$(document).ready(function(){
+$(document).ready(function(){ 
+ 
   $("form#pizzaSizeAndToppings").submit(function(event){
     event.preventDefault();
-      const userPizzaSize = $("#pizza-size").val();
-      console.log(userPizzaSize);
-      let userPizzaToppings = [];
-      $("input:checkbox[name=pizza-toppings]:checked").each(function(){
-        const PizzaToppings = $(this).val();
-        userPizzaToppings.push(PizzaToppings)
-        console.log(userPizzaToppings);
-      });
-  });
-});
+      
+    const userPizzaSize = $("#pizza-size").val(); 
 
-  
+    let userPizzaToppings = [];
+    $("input:checkbox[name=pizza-toppings]:checked").each(function(){
+    const userPizzaToppingSelection = $(this).val();
+    let userPizzaToppings = userPizzaToppingSelection;
+    console.log(userPizzaToppings);
+    })
+    let newUserPizza = new Pizza(userPizzaSize, userPizzaToppings,0)
+    console.log(newUserPizza);
     
-
+    }); 
+  });
